9dd15ed34330c7553bde5125c4bf68bd1471fdb2
Tanmay Patil <92677342+TanmayPatil105@users.noreply.github.com>
2022-06-05 10:31:57 +0530

    Update diff.h

--- diff.h
+++ diff.h
@@ -4,14 +4,16 @@ int checkFiles(char *file1,char *file2);
 
 int NoOfLines(char* file);
 
-void diff(char *file1,char *file2);
+void getFileCreationTime(char *path);
+
+void diff(char *file1,char *file2,char *arg);
 
 void LinesToArray(char **arr1,char **arr2,int l1,int l2,FILE *fptr1,FILE *fptr2);
 
 void LCSmatrix(char **arr1,char **arr2,int **LCS,int **BACK,int l1,int l2);
 
-void BackTrack(char **arr1,char **arr2,int **LCS,int **BACK,int x,int y);
+void NormalDiff(char **arr1,char **arr2,int **LCS,int **BACK,int x,int y);
 
-void BackTrackR(char **arr1,char **arr2,int **LCS,int **BACK,int x,int y);
+void UnionDiff(char **arr1,char **arr2,int **LCS,int **BACK,int x,int y);
 
 void printhelp();
