14569a769518777c2531316c09c0d8b29986e846
Tanmay Patil <tanmaynpatil105@gmail.com>
2022-12-14 16:09:11 +0530

    Fixed runs

--- test/test.sh
+++ test/test.sh
@@ -1,23 +1,16 @@
 #!/bin/bash
 
 make clean
-make
 
+make
 
 ./diff text1 text2 > output1
 
 diff text1 text2 > output2
 
-# echo "$output1"
-
 if [ $(diff -s output1 output2 | wc -l) -eq 1 ]
 then
-	echo -e "\nTest cases passed!"
-else
-	echo -e "\nFailed to Pass test cases!"
-    make clean
-    exit 1
+    echo -e "\nTest cases Passed!!"
+else  
+    echo -e "\nTest cases Failed!!"
 fi
-
-[ -f output1 ] && rm output1
-[ -f output2 ] && rm output2
